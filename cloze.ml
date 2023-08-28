2452e4bc5987550bcf45f4a05294aadf1a56b8e5
Guillaume Huysmans <ghuysmans99@gmail.com>
2023-08-28 18:39:54 +0200

    WIP optimize string concatenation

--- cloze.ml
+++ cloze.ml
@@ -4,28 +4,42 @@ type t =
   | Mask of string * t list * string option
 
 let rec solution = function
-  | Str s -> s
-  | Cat l -> String.concat "" (List.map solution l)
-  | Mask (_, l, _) -> solution (Cat l) (* TODO unfold? *)
+  | Str _ as t -> t
+  | Cat l | Mask (_, l, _) -> Cat (List.map solution l)
 
 let rec to_string = function
-  | Str s -> s
-  | Cat l -> String.concat "" (List.map to_string l)
-  | Mask (m, l, h) ->
-    "{{" ^ m ^ "::" ^
-    to_string (Cat l) ^
-    (Option.value ~default:"" (Option.map ((^) "::") h)) ^
-    "}}"
+  | Str _ as t -> t
+  | Cat l -> Cat (List.map to_string l)
+  | Mask (m, l, h) -> Cat [
+    Str "{{"; Str m; Str "::";
+    to_string (Cat l);
+    Str (Option.value ~default:"" (Option.map ((^) "::") h));
+    Str "}}"
+  ]
 
 let rec delete mask = function
-  | Str s -> s, []
+  | Str _ as t -> t, []
   | Cat l ->
-    let s, a = List.split (List.map (delete mask) l) in
-    String.concat "" s, List.concat a
+    let t, a = List.split (List.map (delete mask) l) in
+    Cat t, List.concat a
   | Mask (m, l, h) when mask = None || mask = Some m ->
-    "[" ^ Option.value ~default:"..." h ^ "]",
+    Cat [Str "["; Str (Option.value ~default:"..." h); Str "]"],
     [solution (Cat l)] (* FIXME test with nested masks *)
   | Mask (_, l, _) -> delete mask (Cat l)
+
+let concat t =
+  let rec f acc = function
+    | Mask _ -> failwith "FIXME eliminate this case using a GADT"
+    | Str s -> s :: acc
+    | Cat l -> List.fold_right (fun x acc -> f acc x) l acc
+  in
+  String.concat "" (f [] t)
+
+let solution t = concat (solution t)
+let to_string t = concat (to_string t)
+let delete mask t =
+  let t, a = delete mask t in
+  concat t, List.map concat a
 
 let rec masks = function
   | Str _ -> []
