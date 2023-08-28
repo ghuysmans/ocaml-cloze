a79d66e3132360662b4392e322b0fcee8cba3758
Guillaume Huysmans <ghuysmans99@gmail.com>
2023-08-28 20:09:51 +0200

    Switch to dune, splitting lib and tests

---
+++ tests/test.ml
@@ -0,0 +1,45 @@
+open Cloze
+
+let one = Cat [
+  Str "Canberra was founded in ";
+  Mask ("c1", [Str "1913"], None);
+  Str "."
+]
+
+let two = Cat [
+  Mask ("c2", [Str "Canberra"], None);
+  Str " was founded in ";
+  Mask ("c1", [Str "1913"], None);
+  Str "."
+]
+
+let hint = Cat [
+  Mask ("c1", [Str "Canberra"], Some "city");
+  Str " was founded in 1913.";
+]
+
+let nested = Cat [
+  Mask ("c1", [Str "Canberra was "; Mask ("c2", [Str "founded"], None)], None);
+  Str " in 1913.";
+]
+
+let rep x n =
+  let rec f acc = function
+    | 0 -> acc
+    | n -> f (Cat [x; acc]) (n - 1)
+  in
+  f (Str "") n
+
+let () =
+  let f t =
+    Printf.printf "%S\n" (to_string t);
+    delete None t :: delete_each t |> List.iter (fun (s, l) ->
+      Printf.printf "- %S: %s\n" s (String.concat ", " l)
+    )
+  in
+  (* FIXME check the output values! *)
+  f one;
+  f two;
+  f hint;
+  f nested;
+  ignore (solution (rep (Str "x") 50000))
